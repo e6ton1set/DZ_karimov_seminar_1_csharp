8f05b1c58c7fa28ce9be8abe4509f2e96d032c1a
Ruslan Karimov <ru-ka-@mail.ru>
2022-09-04 17:22:30 +0500

    added example001

--- Example001/Program.cs
+++ Example001/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.WriteLine("Введите первое число");
+int num1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+int num2 = int.Parse(Console.ReadLine());
+
+    if (num1 > num2)
+{
+        Console.WriteLine($"Чсило {num1} является большим, а число {num2} меньшим");
+}
+    else
+{
+        Console.WriteLine($"Чсило {num2} является большим, а число {num1} меньшим");
+}
