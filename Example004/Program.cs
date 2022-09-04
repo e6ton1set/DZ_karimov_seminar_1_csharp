db8a93593110979cf38d74e72bdcf4ec77fe49f7
Ruslan Karimov <ru-ka-@mail.ru>
2022-09-04 20:51:59 +0500

    added example004

---
+++ Example004/Program.cs
@@ -0,0 +1,8 @@
+﻿//Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+    
+for (int index = 1; index <= number; index++)
+    if (index % 2 == 0)
+        Console.WriteLine(index);
